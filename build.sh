7d75104dadcc9d626e73107fec725f1f635d1340
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-08-03 18:36:56 +0000

    s

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 set -xm
 gcc main.cpp -o Scriptix.exe -lstdc++
+echo Successfly Compiled
--- test/consoleTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "../Scriptix/scriptix.h"
-#include "../Scriptix/scriptix.cpp"
-
-int main() {
-	string name = input("Enter your name: ");
-	string age = input("Enter your age: ");
-    cout << "Hello," << name << "! You age " << age << " years old." << endl;
-	return 0;
-}

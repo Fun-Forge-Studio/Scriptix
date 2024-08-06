c3cfd16d8990e9830367514bf8d0d3ec9b76efa7
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-08-06 00:05:42 +0000

    new

--- scriptix/scriptix.c
+++ scriptix/scriptix.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include "scriptix.h"
 
 void TypeLine(String msg) {
     printf("%s\n", msg);
+}
+
+void input(char *prompt, char *buffer, int bufferSize) {
+    printf("%s", prompt);
+    if(fgets(buffer, bufferSize, stdin) != NULL) {
+        // Remove newline character if persent
+        size_t len = strlen(buffer);
+        if (len > 0 && buffer[len-1] == '\n') {
+            buffer[len-1] = '\0';
+        }
+    } else {
+        // Handle input error
+        fprintf(stderr, "Error: Invalid input provided. %s\n", __FILE__);
+    }
 }
 
 void GetError(SRESULT RESULT) {
@@ -11,7 +26,7 @@ void GetError(SRESULT RESULT) {
             fprintf(stderr, "Error: Invalid input provided. %i (code: %d)\n", __LINE__, RESULT);
             break;
         case FILE_NOT_FOUND:
-			fprintf(stderr, "Error: File not found (code: %d)\n", RESULT);
+            fprintf(stderr, "Error: File not found (code: %d)\n", RESULT);
             break;
         case CONNECTION_FAILED:
             fprintf(stderr, "Error: Connection failed. (code: %d)\n", RESULT);
